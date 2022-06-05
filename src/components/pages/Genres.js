71b3ca3288b90c237a3b6d6cd204197e7a1aa47f
Ceavin Rufus <16521304@std.stei.itb.ac.id>
2022-06-05 15:40:28 +0700

    page fraction on genres

--- src/components/pages/Genres.js
+++ src/components/pages/Genres.js
@@ -8,10 +8,10 @@ import { useParams } from "react-router-dom";
 
 function Genres() {
   const [results, setResults] = useState([]);
+  const [page, setPage] = useState(1);
+  const [maxPage, setMaxPage] = useState(500);
   const { category, genre } = useParams();
   const [genres, setGenres] = useState([]);
-
-  const movies = [];
 
   // Genre list
   useEffect(() => {
@@ -20,25 +20,47 @@ function Genres() {
       .then((genres_data) => {
         setGenres(genres_data.genres);
       });
-  }, []);
+  }, [category]);
 
   // Movie filtered by genre(s)
   useEffect(() => {
     fetch(
-      `${apiConfig[category].DISCOVER_GENRE_URL}${genre}&api_key=9fee2dfca9fac3b1049c2bca2752291c`
+      `${apiConfig[category].DISCOVER_GENRE_URL}${genre}&api_key=9fee2dfca9fac3b1049c2bca2752291c&page=${page}`
     )
       .then((res) => res.json())
       .then((data) => {
-        if (!data.errors) {
-          console.log(data.results);
+        if (!data.errors && !isNaN(genre)) {
           setResults(data.results);
+          if (data.total_pages > 500) {
+            setMaxPage(500);
+          } else if (data.total_pages === 0) {
+            setMaxPage(1);
+          } else {
+            setMaxPage(data.total_pages);
+          }
         } else {
           setResults([]);
+          setMaxPage(500);
         }
       });
-  }, [category, genre]);
+  }, [category, genre, page]);
+
   const genreObj = genres.find((e) => e.id === parseInt(genre));
-  console.log(category);
+
+  const onPreviousPage = () => {
+    if (page === 1) {
+      setPage(maxPage);
+    } else setPage(page - 1);
+  };
+
+  const onNextPage = () => {
+    if (page === maxPage) {
+      setPage(1);
+    } else {
+      setPage(page + 1);
+    }
+  };
+
   return (
     <>
       <div className="background">
@@ -51,7 +73,22 @@ function Genres() {
                 <div className="header">
                   <h2 className="heading">{genreObj.name}</h2>
                 </div>
-
+                <div className="button-wrapper">
+                  <button
+                    className="left-arrow-button"
+                    onClick={onPreviousPage}
+                  >
+                    <i class="fa-solid fa-arrow-left"></i>
+                  </button>
+                  <h2 className="page-fraction">
+                    {"\u00A0"}Page {page}/{maxPage}
+                    {"\u00A0"}
+                    {"\u00A0"}
+                  </h2>
+                  <button className="right-arrow-button" onClick={onNextPage}>
+                    <i class="fa-solid fa-arrow-right"></i>
+                  </button>
+                </div>
                 <div className="movie-container">
                   {results.map((movie) => (
                     <Movie
@@ -70,15 +107,7 @@ function Genres() {
                 <h2 className="no-results">404 Not Found</h2>
               </>
             ) : (
-              <div className="movie-container">
-                {movies.map((movie) => (
-                  <Movie
-                    movie={movie}
-                    key={movie.id}
-                    card_type={"non-watchlist-" + category}
-                  />
-                ))}
-              </div>
+              <div className="movie-container"></div>
             )}
           </div>
         </div>
